f15943829573419d2fe0dd50478083c06e0f03e3
Ppito <ppito91@gmail.com>
2016-11-15 22:46:37 +0100

    Create factory by type of handler

---
+++ Container/ConsoleHandlerFactory.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ppito
+ * Date: 11/15/2016
+ * Time: 8:28 PM
+ *
+ * @link      https://github.com/Ppito/zf3-whoops for the canonical source repository
+ * @copyright Copyright (c) 2016 Mickael TONNELIER.
+ * @license   https://github.com/Ppito/zf3-whoops/blob/master/LICENSE.md The MIT License
+ */
+
+namespace WhoopsErrorHandler\Container;
+
+use Interop\Container\ContainerInterface;
+use Whoops\Handler\Handler;
+use Whoops\Handler\PlainTextHandler as WhoopsConsoleHandler;
+
+class ConsoleHandlerFactory {
+
+    /**
+     * Invoke Console Handler
+     *
+     * @param ContainerInterface $container
+     * @returns Handler
+     */
+    public function __invoke(ContainerInterface $container) {
+
+        $config = $container->has('config') ? $container->get('config') : [];
+        $config = isset($config['whoops']) ? $config['whoops'] : [];
+
+        $pageHandler = new WhoopsConsoleHandler();
+
+        $this->showTrace($pageHandler, $config);
+
+        return $pageHandler;
+    }
+
+    /**
+     * Inject an editor into the whoops configuration.
+     *
+     * @param WhoopsConsoleHandler $handler
+     * @param array|\ArrayAccess   $config
+     * @throws \InvalidArgumentException for an invalid show trace option.
+     */
+    private function showTrace(WhoopsConsoleHandler $handler, $config) {
+
+        if (!isset($config['show_trace']) || !isset($config['show_trace']['cli_display'])) {
+            return;
+        }
+
+        $show_trace = $config['show_trace']['cli_display'];
+
+        if (! is_bool($show_trace)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Whoops show trace option must be a boolean; received "%s"',
+                (is_object($show_trace) ? get_class($show_trace) : gettype($show_trace))
+            ));
+        }
+        $handler->addTraceToOutput($show_trace);
+    }
+}
---
+++ Container/PageHandlerFactory.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ppito
+ * Date: 11/15/2016
+ * Time: 8:29 PM
+ *
+ * @link      https://github.com/Ppito/zf3-whoops for the canonical source repository
+ * @copyright Copyright (c) 2016 Mickael TONNELIER.
+ * @license   https://github.com/Ppito/zf3-whoops/blob/master/LICENSE.md The MIT License
+ */
+
+namespace WhoopsErrorHandler\Container;
+
+use Interop\Container\ContainerInterface;
+use Whoops\Handler\Handler;
+use Whoops\Handler\PrettyPageHandler as WhoopsPageHandler;
+
+class PageHandlerFactory {
+
+    /**
+     * Invoke Page Handler
+     *
+     * @param ContainerInterface $container
+     * @returns Handler
+     */
+    public function __invoke(ContainerInterface $container) {
+
+        $config = $container->has('config') ? $container->get('config') : [];
+        $config = isset($config['whoops']) ? $config['whoops'] : [];
+
+        $pageHandler = new WhoopsPageHandler();
+
+        $pageHandler->setApplicationPaths([__FILE__]);
+        $this->injectEditor($pageHandler, $config, $container);
+
+        return $pageHandler;
+    }
+
+    /**
+     * Inject an editor into the whoops configuration.
+     *
+     * @param WhoopsPageHandler  $handler
+     * @param array|\ArrayAccess $config
+     * @param ContainerInterface $container
+     * @throws \InvalidArgumentException for an invalid editor definition.
+     */
+    private function injectEditor(WhoopsPageHandler $handler, $config, ContainerInterface $container) {
+
+        if (! isset($config['editor'])) {
+            return;
+        }
+
+        $editor = $config['editor'];
+        if (!is_callable($editor) && !is_string($editor)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Whoops editor must be a string editor name, string service name, or callable; received "%s"',
+                (is_object($editor) ? get_class($editor) : gettype($editor))
+            ));
+        }
+
+        if (is_string($editor) && $container->has($editor)) {
+            $editor = $container->get($editor);
+        }
+
+        $handler->setEditor($editor);
+    }
+}
